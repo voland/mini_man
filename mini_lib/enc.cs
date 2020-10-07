61fde594059116b806591c1f0e72430204047eab
Arkadiusz Gil <voland83@gmail.com>
2020-10-07 15:28:22 +0200

    needed exception

--- mini_lib/enc.cs
+++ mini_lib/enc.cs
@@ -3,7 +3,6 @@ using System.Text;
 
 namespace mini_lib {
     public class Enc {
-
         static public Encoding _enc = null;
         static public Encoding e {
             get {
@@ -12,6 +11,8 @@ namespace mini_lib {
                     if (_enc == null)
                         _enc = CodePagesEncodingProvider.Instance.GetEncoding(28592);
                 }
+				if ( _enc == null) 
+					throw new Exception("Can not get encoding try to set \"e\" value externaly like Enc.e = Encoding.GetEncoding(28592);");
                 return _enc;
             }
             set {
